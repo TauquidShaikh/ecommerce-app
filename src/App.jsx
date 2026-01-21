f34a025b1fcba11aae060a0d8cf09bb2cad54dd9
TauquidShaikh <tauquidkhatik@gmail.com>
2026-01-22 00:20:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vm] md:px-[7vm] lg:px-[9vm]'>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Collection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Collection() {
+  return (
+    <div>
+      <h1>Collection Page</h1>
+    </div>
+  )
+}
+
+export default Collection
